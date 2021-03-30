b81d2261936681b7c63275f369fd0fddd9da604a
Lalit Radadiya <lalit.radadiya@gatewaygroup.com>
2021-03-30 16:48:17 +0530

    d

---
+++ HRM.DAL/Database/DatabaseContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HRM.DAL.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        const string connectionString = "Server=azuredbautomotive.database.windows.net;Database=DotNetcore;Integrated Security=true;User ID=LalitAkRadadiya;Password=Gateway123;";
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<tblEmployees> tblEmployee { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer();
+            }
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+                optionsBuilder.UseSqlServer(configuration["ConnectionString:DefaultConnection"]);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<tblEmployees>().Property(e => e.isManager).HasDefaultValue(false);
+        }
+       
+    }
+}
